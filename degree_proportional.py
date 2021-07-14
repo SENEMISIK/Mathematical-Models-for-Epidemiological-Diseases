ce9aa364b4e375e08bc015b51f2150e5387e3b6d
Senem Isik <77026721+SENEMISIK@users.noreply.github.com>
2021-07-14 11:17:09 -0700

    budget simo

--- degree_proportional.py
+++ degree_proportional.py
@@ -59,7 +59,6 @@ def calculateFinalInfection(numOfInfectedNodes, numOfNodes, numOfEdges, numOfTri
     neighbors_per_node = tuples_to_dict(graph, numOfNodes)
     infected_nodes = find_entire_connection(random.sample([i for i in range(0, numOfNodes)], numOfInfectedNodes), neighbors_per_node)
     num_infected.append(len(infected_nodes))
-    print(infected_nodes)
   return np.mean(num_infected)
 
 
