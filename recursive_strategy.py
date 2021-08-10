c9dab33147b25fd82fbd19ebc357422a8f85b34f
Senem Isik <77026721+SENEMISIK@users.noreply.github.com>
2021-08-10 22:15:19 +0300

    o

--- recursive_strategy.py
+++ recursive_strategy.py
@@ -210,7 +210,7 @@ def findNewGraph(graph_dict, nodes1, nodes2):
 def newStrategy(graph_dict, recovery_rates, budget, gap_threshold, boundary_threshold, node_list):
   gap = measureSpectralGap(graph_dict, node_list, gap_threshold)
   if (gap < gap_threshold) and (len(node_list) > 3):
-    print("spectral partioning")
+    # print("spectral partioning")
     nodes1, nodes2 = sparsest_cut(graph_dict, node_list)
     graph_dict1, len1, graph_dict2, len2 = findNewGraph(graph_dict, nodes1, nodes2)
     # print("beofre" + str(budget))
